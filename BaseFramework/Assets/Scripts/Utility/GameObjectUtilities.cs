5af9b497505e57eac3e1c9893c0ed4e0d6cfac9d
jamesmcn4242 <jamesmcneil42@hotmail.co.uk>
2019-06-08 22:38:23 +0100

    Get components from a specified child object as well

--- BaseFramework/Assets/Scripts/Utility/GameObjectUtilities.cs
+++ BaseFramework/Assets/Scripts/Utility/GameObjectUtilities.cs
@@ -10,7 +10,8 @@ using UnityEngine;
 /// <summary>
 /// Class to hold utility methods and GameObject extension methods
 /// </summary>
-public static class GameObjectUtilities {
+public static class GameObjectUtilities
+{
 
     /// <summary>
     /// Find a child with the specified name
@@ -21,22 +22,41 @@ public static class GameObjectUtilities {
     /// <returns>First instance of a child with the specified name, or null if not found</returns>
     public static GameObject FindChildByName(this GameObject go, string childName)
     {
-        for(int i = 0; i < go.transform.childCount; i++)
+        for (int i = 0; i < go.transform.childCount; i++)
         {
             GameObject child = go.transform.GetChild(i).gameObject;
 
-            if(child.name == childName)
+            if (child.name == childName)
             {
                 return child;
             }
             else
             {
                 GameObject childObj = child.FindChildByName(childName);
-                if(childObj != null)
+                if (childObj != null)
                 {
                     return childObj;
                 }
             }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Get a component from a specified child object
+    /// </summary>
+    /// <typeparam name="T">Component Type</typeparam>
+    /// <param name="go">Calling GameObject</param>
+    /// <param name="childName">Name of child</param>
+    /// <returns>Found component, or null if child doesn't exist or contain component type T</returns>
+    public static T GetComponentFromChild<T>(this GameObject go, string childName) where T : Component
+    {
+        GameObject child = go.FindChildByName(childName);
+
+        if(child != null)
+        {
+            return child.GetComponent<T>();
         }
 
         return null;
