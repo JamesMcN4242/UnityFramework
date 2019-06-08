3a0f953cfc27a8b5b5aaa21ca929b41cfdc501b9
jamesmcn4242 <jamesmcneil42@hotmail.co.uk>
2019-06-08 22:25:24 +0100

    Added class script

--- BaseFramework/Assets/Scripts/Utility/GameObjectUtilities.cs
+++ BaseFramework/Assets/Scripts/Utility/GameObjectUtilities.cs
@@ -7,6 +7,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class to hold utility methods and GameObject extension methods
+/// </summary>
 public static class GameObjectUtilities {
 
     /// <summary>
